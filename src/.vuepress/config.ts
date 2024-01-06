0a6ac46452093e8651db79a3b911a5d36f24a6ff
joeljhou <joeljhou336@gmail.com>
2024-01-06 18:18:27 +0800

    plugin: 必应壁纸/一言描述

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,7 @@
+import { getDirname, path } from "@vuepress/utils";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+
 
 export default defineUserConfig({
   base: "/blog/",
@@ -12,4 +14,10 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
+
+  alias: {
+    // 必应壁纸，一言描述
+    "@theme-hope/modules/blog/components/BlogHero":
+        path.resolve(__dirname, "./components/BlogHero.vue"),
+  },
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -218,4 +218,4 @@ export default hopeTheme({
     //   },
     // },
   },
-});
+},{ custom: true });
